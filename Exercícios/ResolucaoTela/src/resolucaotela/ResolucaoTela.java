f6e9de385300e78424b675be33f9545a9c0cfddf
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-07-11 19:29:00 -0300

    Resolução de tela

---
+++ Exercícios/ResolucaoTela/src/resolucaotela/ResolucaoTela.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resolucaotela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author vinicius.azevedo
+ */
+public class ResolucaoTela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("Sua tela tem resolução " + d.width + " x " + d.height);
+    }
+    
+}
